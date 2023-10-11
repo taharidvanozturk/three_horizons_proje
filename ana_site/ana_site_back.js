28ab1210a3512d234b7f5f81a34d5ce2c329ca63
taharidvanozturk <taharidvanozturk0@gmail.com>
2023-10-11 22:11:41 +0300

    gride dinamik ekleme eklendi

--- ana_site/ana_site_back.js
+++ ana_site/ana_site_back.js
@@ -1,61 +1,26 @@
-const express = require("express");
-const axios = require("axios");
-const crypto = require("crypto");
-const { createHmac } = require("crypto");
-const app = express();
+axios.get('https://btk-maraton.obs.myhuaweicloud.com/')
+  .then(function (response) {
+    const xmlContent = response.data; // XML içeriği
 
-const accessKey = "UEZH2HJM6NR0GWUGKTHT";
-const secretKey = "b9fp5CZh6iwZDDuC0CN6DKeuRIx5n5o6uMDBoYVC";
+    // XML içeriğini işlemek için bir DOMParser oluşturun
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlContent, 'application/xml');
 
-// Define currentTime as GMT+3
-const currentTimeOffset = 3; // Adjust this offset as needed
-let currentTime = new Date();
-currentTime.setUTCHours(currentTime.getUTCHours() + currentTimeOffset);
-currentTime = currentTime.toUTCString();
-console.log("Current Time:", currentTime);
-app.post('/getImagesList/.png', (req, res) => {
-    let objectName =".png";
-    fetchFromOBS(currentTime, objectName, res);
-});
+    // <Key> etiketlerini içeren tüm öğeleri seçin
+    const keyElements = xmlDoc.querySelectorAll('Key');
+    const imageContainer = document.getElementById('image-container');
 
-function fetchFromOBS(currentTime, objectName, res) {
-    let stringToSign =
-        "GET\n\n" +
-        "\n" +
-        currentTime +
-        "\n/btk-maraton/" +
-        objectName;
+    // Her <Key> öğesini işleyerek resim URL'lerini alın ve resimleri sayfada gösterin
+    keyElements.forEach(function (keyElement) {
+      const imageUrl = keyElement.textContent;
+      const imgElement = document.createElement('img');
+      imgElement.src = `https://btk-maraton.obs.myhuaweicloud.com/${imageUrl}`;
+      imgElement.classList.add('col-md-4'); // Bootstrap 4 için bir sütun sınıfı ekleyin
+      imageContainer.appendChild(imgElement);
+      console.log("Image URL:", imgElement.src);
 
-    let hmac = createHmac("sha1", secretKey);
-    hmac.update(stringToSign);
-    let signature = hmac.digest("base64");
-
-    console.log("Preparing to fetch image:", objectName);
-
-    axios({
-        method: "get",
-        url: `https://btk-maraton.obs.tr-west-1.myhuaweicloud.com/.png`,
-      /*  headers: {
-    'Content-Type': 'image/png',  // Corrected Content-Type header
-            Authorization: "OBS " + accessKey + ":" + signature,
-            Date: currentTime,
-        },*/
-        responseType: 'stream'  // important to set this for streaming
-    })
-    .then(function (response) {
-        console.log("Received response from OBS:", response.status);
-        
-        // pipe the image data directly to the client
-        response.data.pipe(res);
-    })
-    .catch(function (error) {
-        console.error("Error fetching the image:", error);
-        res.status(500).send('Failed to fetch the image.');
     });
-}
-    
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
-
-console.log("Server started.");
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
